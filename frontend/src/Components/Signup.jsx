fe9e30e9ed9832ebd055de9feeb927e12c89f771
pc <pc@pop-os.localdomain>
2022-06-12 19:08:33 +0530

    added assets folder and made changes to signup.jsx

---
+++ frontend/src/Components/Signup.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles/Signup.css'
+
+const Signup = () => {
+  return (
+    <div className="Signup">
+      <form className='card'>
+        <input type="text" placeholder='Username'></input>
+        <input type="email" placeholder='Email'></input>
+        <input type="password" placeholder='password'></input>
+        <button className='style_button'>SignUp</button>
+      </form>
+    </div>
+    
+  );
+}
+
+export default Signup
